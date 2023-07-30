ec8ee61994abd7ba50202c36c41e3854f860c376
narcisolobo <narcisolobo@gmail.com>
2023-07-29 21:56:19 -0700

    removed unused import

--- factory/blueprints/auth/routes.py
+++ factory/blueprints/auth/routes.py
@@ -1,5 +1,5 @@
 from flask import flash, redirect, render_template, request, url_for
-from flask_login import current_user, login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user
 
 from factory.blueprints.auth import auth
 from factory.extensions import bcrypt, db
